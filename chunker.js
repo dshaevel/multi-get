72874f8d4a0e18f3f42239e1ec14cb36e38b450d
David Shaevel <david@davidshaevel.com>
2018-04-08 14:41:25 -0500

    write the chunks to a file

--- chunker.js
+++ chunker.js
@@ -1,20 +1,25 @@
 const async = require('async');
+const fs = require('fs');
 const http = require('http');
 const { URL } = require('url');
 const utils = require('./utils');
 
 const testUrl = 'http://91vfv5w.bwtest-aws.pravala.com/384MB.jar';
+const chunkDataArray = [];
+let filename = '';
 
 function prepareHeaders(program) {
-    const urlStr = testUrl;
-    // const urlStr = program.args[0];
+    // const urlStr = testUrl;
+    const urlStr = program.args[0];
     const url = new URL(urlStr);
     const pathname = url.pathname.substr(url.pathname.lastIndexOf('/') + 1);
-    const filename = program.output ? program.output :
+    filename = program.output ? program.output :
         pathname === '' ? 'index.html' : pathname;
     const numberOfChunks = program.chunks ? program.chunks : 4;
 
     process.stdout.write('Downloading first ' + numberOfChunks + ' chunks of \'' + urlStr + '\' to \'' + filename + '\'');
+
+    // TODO: is there a better way to populate the optionsArray?
     const optionsArray = [];
     for (let x = 1; x <= numberOfChunks; x++) {
         const options = {
@@ -36,25 +41,29 @@ function makeRequests(params) {
 
     if (program.parallel) {
         process.stdout.write(' in parallel\n');
-        async.each(optionsArray, download, done);
+        async.eachOf(optionsArray, download, done);
     } else {
         process.stdout.write(' in serial\n');
-        async.eachSeries(optionsArray, download, done);
+        async.eachOfSeries(optionsArray, download, done);
     }
 }
 
-function download(opt, callback) {
+function download(opt, index, callback) {
     const req = http.request(opt, (res) => {
         // console.log('HEADERS: ' + JSON.stringify(res.headers, null, 2));
+        res.setEncoding('binary');
+        const chunkData = [];
 
         if (res.statusCode >= 400) {
             console.error('[ERROR] Unexpected response from server: %s %s', res.statusCode, res.statusMessage);
             process.exit(1);
         }
-        res.on('data', (chunkData) => {
+        res.on('data', (data) => {
+            chunkData.push(Buffer.from(data, 'binary'));
         });
         res.on('end', () => {
             process.stdout.write('.');
+            chunkDataArray[index] = Buffer.concat(chunkData);
             callback();
         });
     });
@@ -68,7 +77,14 @@ function download(opt, callback) {
 }
 
 function done() {
-    console.log('done');
+    const combinedBuffer = Buffer.concat(chunkDataArray);
+    fs.writeFile(filename, combinedBuffer, function(err) {
+        if (err) {
+            console.error('[ERROR] An error occurred saving the file: %s', err.message);
+            process.exit(1);
+        }
+        console.log('done');
+    });
 }
 
 module.exports = {
