f31e53d9c8a2f659fee00d2effa8f666614ec0f2
David Shaevel <david@davidshaevel.com>
2018-04-07 20:13:43 -0500

    start building command options

--- multiget.js
+++ multiget.js
@@ -5,4 +5,12 @@ var program = require('commander');
 
 program
     .version(this.version)
+    .usage('[options] <url>')
+    .option('-o, --output <file>', 'write output to <file> instead of default')
+    .option('-p, --parallel', 'download chunks in parallel instead of sequentially')
     .parse(process.argv);
+
+if (program.args.length === 0) {
+    program.outputHelp();
+    process.exit(1);
+} 
