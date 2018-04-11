838ffe6683bbb9aaf81e43f1ae10f78cf70d5226
David Shaevel <david@davidshaevel.com>
2018-04-10 20:57:21 -0500

    Update usage information

--- multiget.js
+++ multiget.js
@@ -7,13 +7,43 @@ const chunker = require('./chunker');
 
 program
     .version(this.version)
+    .description(
+        'A command for downloading a file in chunks.\n\n' +
+        '  By default, multiget will request the first 4 MiB of a file in 1 MiB chunks, and write the result to disk.'
+    )
     .usage('[options] <url>')
     .option('-c, --chunks <#>', 'number of chunks to use instead of default 4')
     .option('-d, --debug', 'enable debug mode')
     .option('-o, --output <file>', 'write output to <file> instead of default')
     .option('-p, --parallel', 'download chunks in parallel instead of sequentially')
-    .option('-s, --size <#>', 'size of chunks to use, in MiB, instead of default 1')
-    .parse(process.argv);
+    .option('-s, --size <#>', 'size of chunks to use, in MiB, instead of default 1');
+
+program.on('--help', () => {
+    console.log();
+    console.log('  Examples:');
+    console.log();
+    console.log('    DEFAULT');
+    console.log('      Download first 4 chunks in 1 MiB chunks in serial:');
+    console.log('      $ ./multiget.js http://91vfv5w.bwtest-aws.pravala.com/384MB.jar');
+    console.log();
+    console.log('    -c, --chunks');
+    console.log('      Download first 10 chunks in 1 MiB chunks in serial:');
+    console.log('      $ ./multiget.js -c 10 http://91vfv5w.bwtest-aws.pravala.com/384MB.jar');
+    console.log();
+    console.log('    -s, --size');
+    console.log('      Download first 10 chunks in 5 MiB chunks in serial:');
+    console.log('      $ ./multiget.js -c 10 -s 5 http://91vfv5w.bwtest-aws.pravala.com/384MB.jar');
+    console.log();
+    console.log('    -p, --parallel');
+    console.log('      Download first 10 chunks in 5 MiB chunks in parallel:');
+    console.log('      $ ./multiget.js -c 10 -s 5 -p http://91vfv5w.bwtest-aws.pravala.com/384MB.jar');
+    console.log();
+    console.log('    -o, --output');
+    console.log('      Download first 10 chunks in 5 MiB chunks in parallel and write output to \'download.jar\':');
+    console.log('      $ ./multiget.js -c 10 -s 5 -p -o download.jar http://91vfv5w.bwtest-aws.pravala.com/384MB.jar');
+    console.log();
+});
+program.parse(process.argv);
 
 if (program.args.length === 0 || program.args.length > 1) {
     program.outputHelp();
